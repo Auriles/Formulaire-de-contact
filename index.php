0b4c64d69247fcd1adc46fbae7d887ee914f4b57
Auriles El Haddad <auriles0404@gmail.com>
2017-03-04 15:29:19 +0100

    Envoi de mail

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   $firstname = $name = $email = $phone = $message = "";
   $firstnameError = $nameError = $emailError = $phoneError = $messageError = "";
   $isSuccess = false;
+  $emailTo = "auriles0404@gmail.com";
 
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -12,35 +13,48 @@
     $phone = verifyInput($_POST["phone"]);
     $message = verifyInput($_POST["message"]);
     $isSuccess = true;
+    $emailText = "";
 
     if(empty($firstname)) {
       $firstnameError = " Je veux connaitre ton prénom !";
       $isSuccess = false;
+    } else {
+      $emailText .= "FirstName : $firstname\n";
     }
 
     if(empty($name)) {
       $nameError = " Et oui je veux tout savoir de toi, même ton nom !";
       $isSuccess = false;
-    }
-
-    if(empty($message)) {
-      $messageError = " Je veux recevoir quelque chose quand même !";
-      $isSuccess = false;
+    } else {
+      $emailText .= "Name : $name\n";
     }
 
     if(!isEmail($email)) {
       $emailError = " Tu essayes de me rouler? C'est pas un email ça !";
       $isSuccess = false;
+    } else {
+      $emailText .= "Email : $email\n";
     }
 
     if(!isPhone($phone)) {
       $phoneError = " Que des chiffres et des espaces stp...";
       $isSuccess = false;
+    } else {
+      $emailText .= "Phone : $phone\n";
     }
 
-    // if(isSuccess) {
-    //   // Envoi de l'email
-    // }
+    if(empty($message)) {
+      $messageError = " Je veux recevoir quelque chose quand même !";
+      $isSuccess = false;
+    } else {
+      $emailText .= "Message : $message\n";
+    }
+
+    if($isSuccess) {
+      $headers = "From: $firstname $name <$email>\r\nReply-To: $email";
+      mail($emailTo, "Un message de votre site", $emailText, $headers);
+      $firstname = $name = $email = $phone = $message = "";
+    }
 
   }
 
